6047acd6aa9ead20b36b62eb093375901c8e93ae
BookFairDigital <n.imethdm@gmail.com>
2026-09-19 20:25:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,58 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import {getAuth,signInWithEmailAndPassword,signOut,onAuthStateChanged} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import {getFirestore,doc,getDoc,setDoc,collection,getDocs,deleteDoc} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-import {firebaseConfig} from "./firebase-config.js";
-const app=initializeApp(firebaseConfig); export const auth=getAuth(app); export const db=getFirestore(app);
-export {signInWithEmailAndPassword,signOut,onAuthStateChanged,doc,getDoc,setDoc,collection,getDocs,deleteDoc};
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  getDocs,
+  deleteDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+import {
+  firebaseConfig
+} from "./firebase-config.js";
+
+
+/*
+ * Main application
+ */
+const app =
+  initializeApp(firebaseConfig);
+
+export const auth =
+  getAuth(app);
+
+export const db =
+  getFirestore(app);
+
+
+/*
+ * Export config so admin.js can create
+ * student Firebase Auth accounts using
+ * a separate Auth instance.
+ */
+export { firebaseConfig };
+
+
+export {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  getDocs,
+  deleteDoc
+};
